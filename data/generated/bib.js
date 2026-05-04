c6b8e9a60d10c5df7d454f51ad25315e79f6e318
CyanPromise <gxdmjy520@gmail.com>
2026-05-04 11:57:34 +0800

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,138 @@
+﻿const generatedBibEntries = {
+    "bagdasaryan2018backdoor": {
+        "author": "Bagdasaryan, E. and Veit, A. and Hua, Y. and Estrin, D. and Shmatikov, V.",
+        "booktitle": "NeurIPS 2018",
+        "doi": "10.48550/arXiv.1807.00459",
+        "keywords": "federated learning, backdoor attack, model poisoning, attack",
+        "pages": "1--12",
+        "series": "NeurIPS",
+        "title": "How To Backdoor Federated Learning",
+        "type": "inproceedings attack",
+        "year": "2018"
+    },
+    "cao2021fltrust": {
+        "author": "Cao, X. and Fang, M. and Liu, J. and Gong, N.Z.",
+        "booktitle": "NDSS 2021",
+        "doi": "10.14722/NDSS.2021.24434",
+        "keywords": "federated learning, robust aggregation, trust, defense",
+        "series": "NDSS",
+        "title": "FLTrust: Byzantine-robust Federated Learning via Trust Bootstrapping",
+        "type": "inproceedings centralized_defense",
+        "year": "2021"
+    },
+    "gabrielli2023dflsurvey": {
+        "author": "Gabrielli, E. and Pica, G. and Tolomei, G.",
+        "doi": "10.48550/arXiv.2308.04604",
+        "keywords": "decentralized federated learning, distributed optimization, survey",
+        "series": "arXiv preprint",
+        "title": "A Survey on Decentralized Federated Learning",
+        "type": "article survey",
+        "year": "2023"
+    },
+    "hallaji2024security": {
+        "author": "Hallaji, E. and Razavi-Far, R. and Saif, M. and Wang, B. and Yang, Q.",
+        "doi": "10.1109/TBDATA.2024.3362191",
+        "journal": "IEEE Transactions on Big Data",
+        "keywords": "decentralized federated learning, security, privacy, survey",
+        "number": "2",
+        "pages": "194--213",
+        "series": "IEEE TBD",
+        "title": "Decentralized Federated Learning: A Survey on Security and Privacy",
+        "type": "article survey",
+        "volume": "10",
+        "year": "2024"
+    },
+    "he2022clippedgossip": {
+        "author": "He, L. and Karimireddy, S.P. and Jaggi, M.",
+        "doi": "10.48550/arXiv.2202.01545",
+        "keywords": "decentralized federated learning, robust aggregation, byzantine resilience",
+        "series": "arXiv preprint",
+        "title": "Byzantine-Robust Decentralized Learning via ClippedGossip",
+        "type": "article decentralized_defense",
+        "year": "2022"
+    },
+    "ma2022dpbfl": {
+        "author": "Ma, X. and Sun, K. and Lou, X. and Liu, Y. and Hu, Q.",
+        "doi": "10.1109/TPDS.2022.3167434",
+        "journal": "IEEE Transactions on Parallel and Distributed Systems",
+        "keywords": "federated learning, byzantine robustness, differential privacy, defense",
+        "number": "11",
+        "pages": "2945--2957",
+        "series": "IEEE TPDS",
+        "title": "Differentially Private Byzantine-Robust Federated Learning",
+        "type": "article centralized_defense",
+        "volume": "33",
+        "year": "2022"
+    },
+    "mao2025ragd": {
+        "author": "Mao, Y. and Diggavi, S. and Tabuada, P.",
+        "doi": "10.1109/TAC.2024.3424693",
+        "journal": "IEEE Transactions on Automatic Control",
+        "keywords": "decentralized optimization, RAGD, RWSE, local poisoning, defense",
+        "number": "1",
+        "pages": "81--96",
+        "series": "IEEE TAC",
+        "title": "Decentralized Optimization Resilient Against Local Data Poisoning Attacks",
+        "type": "article decentralized_defense",
+        "volume": "70",
+        "year": "2025"
+    },
+    "rodriguez2022survey": {
+        "author": "Rodr\u00edguez-Barroso, N. and Jim\u00e9nez-L\u00f3pez, D. and Luz\u00f3n, M.V. and Herrera, F. and Mart\u00ednez-C\u00e1mara, E.",
+        "doi": "10.1016/j.inffus.2022.09.011",
+        "journal": "Information Fusion",
+        "keywords": "federated learning, security, poisoning attacks, survey",
+        "pages": "148--173",
+        "series": "Information Fusion",
+        "title": "Survey on Federated Learning Threats: Concepts, Taxonomy on Attacks and Defences, Experimental Study and Challenges",
+        "type": "article survey",
+        "volume": "90",
+        "year": "2022"
+    },
+    "tolpegin2020datapoison": {
+        "author": "Tolpegin, V. and Truex, S. and Gursoy, M.E. and Liu, L.",
+        "booktitle": "Computer Security \u2013 ESORICS 2020",
+        "doi": "10.1007/978-3-030-58951-6_24",
+        "keywords": "federated learning, data poisoning, adversary, attack",
+        "pages": "480--501",
+        "publisher": "Springer",
+        "series": "ESORICS",
+        "title": "Data Poisoning Attacks Against Federated Learning Systems",
+        "type": "incollection attack",
+        "year": "2020"
+    },
+    "xia2023poisoning": {
+        "author": "Xia, G. and Chen, J. and Yu, C. and Ma, J.",
+        "doi": "10.1109/ACCESS.2023.3238823",
+        "journal": "IEEE Access",
+        "keywords": "federated learning, poisoning attacks, backdoor attacks, survey",
+        "pages": "10708--10722",
+        "series": "IEEE Access",
+        "title": "Poisoning Attacks in Federated Learning: A Survey",
+        "type": "article survey",
+        "volume": "11",
+        "year": "2023"
+    },
+    "yang2024removethenclip": {
+        "author": "Yang, C. and Ghaderi, J.",
+        "booktitle": "AAAI 2024",
+        "doi": "10.1609/aaai.v38i19.30173",
+        "keywords": "decentralized learning, byzantine-robust, aggregation, defense",
+        "pages": "21735--21743",
+        "series": "AAAI",
+        "title": "Byzantine-Robust Decentralized Learning via Remove-then-Clip Aggregation",
+        "type": "inproceedings decentralized_defense",
+        "year": "2024"
+    },
+    "zhang2024balance": {
+        "author": "Zhang, Z. and Fang, M. and Khanduri, P. and Liu, J. and Lu, S. and Liu, Y. and Gong, N.Z.",
+        "booktitle": "ACM CCS 2024",
+        "doi": "10.1145/3658644.3670307",
+        "keywords": "decentralized federated learning, robust aggregation, local similarity, defense",
+        "pages": "3333--3347",
+        "series": "ACM CCS",
+        "title": "Byzantine-Robust Decentralized Federated Learning (BALANCE)",
+        "type": "inproceedings decentralized_defense",
+        "year": "2024"
+    }
+};
